b011a738a0a48154a67843a2b6fab2ec6a122698
Hugo <hugorey@hotmail.es>
2024-11-14 20:00:13 +0100

    Creadas funciones de alto nivel en Extensiones.kt

--- app/src/main/java/com/hugo/simulacroexamen/util/Extensiones.kt
+++ app/src/main/java/com/hugo/simulacroexamen/util/Extensiones.kt
@@ -2,8 +2,32 @@ package com.hugo.simulacroexamen.util
 
 import android.app.Activity
 import android.widget.Toast
+import com.hugo.simulacroexamen.model.Actividad
+import com.hugo.simulacroexamen.model.Pago
 
 
 fun Activity.mostrarMensaje(mensaje: String, tiempo: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, mensaje, tiempo).show()
+}
+
+
+fun ArrayList<Actividad>.filtrar(f: (Actividad) -> Boolean) : ArrayList<Actividad>{
+    val final: ArrayList<Actividad> = arrayListOf()
+
+    this.forEach {
+        if (f(it)) final.add(it)
+    }
+
+    return final
+}
+
+
+fun ArrayList<Pago>.x(f: (Pago) -> Boolean) : Double {
+    var total: Double = 0.0
+
+    this.forEach {
+        if (f(it)) total += it.cantidad
+    }
+
+    return total
 }
