1811001ab1519c0e3cd4789a1dfa4f0211baa2e1
Hugo <hugorey@hotmail.es>
2024-11-14 21:23:36 +0100

    Creadas otras actividades para introducir Tareas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.hugo.simulacroexamen"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hugo.simulacroexamen"
--- app/src/main/java/com/hugo/simulacroexamen/MainActivity.kt
+++ app/src/main/java/com/hugo/simulacroexamen/MainActivity.kt
@@ -1,15 +1,45 @@
 package com.hugo.simulacroexamen
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioGroup
+import android.widget.ToggleButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hugo.simulacroexamen.model.Tarea
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        inicializarComponentes()
+    }
+
+    private fun inicializarComponentes() {
+        val boton: Button = findViewById(R.id.bAnadir)
+
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+
+        boton.setOnClickListener{
+            when(radioGroup.checkedRadioButtonId) {
+                R.id.rbTarea -> {
+                    var intent: Intent = Intent(this, TareaActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.rbCita -> {
+
+                }
+
+                R.id.rbPago -> {
+
+                }
+            }
+        }
 
     }
 }
